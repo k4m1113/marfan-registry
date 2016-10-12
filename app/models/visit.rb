a289db58328cd2a2df5bed9bda7b584ea6002d96
Kamille Delgardo <kamilledelgardo@gmail.com>
2016-10-12 11:57:23 -0700

    model associations

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -4,8 +4,10 @@ class Visit < ActiveRecord::Base
   belongs_to :patient
   has_many :symptoms, inverse_of: :visit, dependent: :destroy
   has_many :hospitalizations
+  has_many :family_members, through: :patient
 
   accepts_nested_attributes_for :symptoms
   accepts_nested_attributes_for :hospitalizations
-  accepts_nested_attributes_for :family_member
+  accepts_nested_attributes_for :family_members
+  accepts_nested_attributes_for :patient
 end
