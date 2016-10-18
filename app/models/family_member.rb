32ee4a0e64664e95ec67bdd3e3b4bea8ced42ad6
Kamille Delgardo <kamilledelgardo@gmail.com>
2016-10-18 16:47:44 -0700

    heroku errors

--- app/models/family_member.rb
+++ app/models/family_member.rb
@@ -4,16 +4,15 @@ class FamilyMember < ActiveRecord::Base
   validates :visit,
     presence: true
   validates :name,
-    format: { with: /\A[a-zA-Z ']+\z/ }
+    format: { with: /\A[a-zA-Z ']+\z/ },
     allow_nil: true
   validates :relationship,
-    inclusion: { in: ["Mother", "Father", "Dad's sibling", "Mom's sibling", "Paternal Grandmother", "Paternal Grandfather", "Maternal Grandmother", "Maternal Grandfather", "Sister", "Brother", "Son", "Daughter"] }
+    inclusion: { in: ["Mother", "Father", "Dad's sibling", "Mom's sibling", "Paternal Grandmother", "Paternal Grandfather", "Maternal Grandmother", "Maternal Grandfather", "Sister", "Brother", "Son", "Daughter"] },
     allow_nil: true
   validates :age,
-    numericality: { greater_than_or_equal_to: 0 }
+    numericality: { greater_than_or_equal_to: 0 },
     allow_nil: true
   validates :living,
-    inclusion: { :in => [true, false] }
+    inclusion: { :in => [true, false] },
     allow_nil: true
-
 end
