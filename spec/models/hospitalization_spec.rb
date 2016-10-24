6b6db88811f3b6cce03cb77535674bc8c227e8c6
Kamille Delgardo <kamilledelgardo@gmail.com>
2016-10-24 12:58:04 -0700

    passing build

--- spec/models/hospitalization_spec.rb
+++ spec/models/hospitalization_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
 describe Hospitalization do
-  it { should have_valid(:visit_id).when(1, 44) }
-  it { should_not have_valid(:visit_id).when(-1, 4.4, nil, false, "one") }
   it { should have_valid(:length_of_stay).when(1, 3, nil) }
   it { should_not have_valid(:length_of_stay).when(-1, false, "Seven") }
 end
