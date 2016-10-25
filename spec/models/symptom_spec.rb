1cc37f780370070ea67e24f174be3f8c17f0ee81
Kamille Delgardo <kamilledelgardo@gmail.com>
2016-10-24 18:00:59 -0700

    passing tests

--- spec/models/symptom_spec.rb
+++ spec/models/symptom_spec.rb
@@ -7,7 +7,6 @@ describe Symptom do
   it { should_not have_valid(:seeded_symptom_id).when(-1, false, nil, "") }
 
   it { should have_valid(:visit_id).when(1, 23) }
-  it { should_not have_valid(:visit_id).when(-1, false, nil, "") }
 
   it { should have_valid(:presence).when(true, false, nil) }
 
