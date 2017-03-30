f2f1dab932891f7e1ce800096af85e9f58101c9f
Kamille Delgardo <kamilledelgardo@gmail.com>
2017-03-30 15:27:53 -0700

    accounting for multiple brand names

---
+++ app/assets/javascripts/components/epicMeds.js
@@ -0,0 +1,19 @@
+function splitNoParen(s){
+    var left= 0, right= 0, A= [],
+    M= s.match(/([^()]+)|([()])/g), L= M.length, next, str= '';
+    for(var i= 0; i<L; i++){
+        next= M[i];
+        if(next=== '(')++left;
+        else if(next=== ')')++right;
+        if(left!== 0){
+            str+= next;
+            if(left=== right){
+                A[A.length-1]+=str;
+                left= right= 0;
+                str= '';
+            }
+        }
+        else A=A.concat(next.match(/([^,]+)/g));
+    }
+    return A;
+}
