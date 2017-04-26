d8131188c749de68fcf38e3b7bcda5441a236811
Kamille Delgardo <kamilledelgardo@gmail.com>
2017-04-25 20:44:46 -0700

    universal show table template

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -1,5 +1,6 @@
 class Medication < ActiveRecord::Base
   include ActiveSupport::NumberHelper
+  attr_accessor :table_headings, :table_body
 
   has_one :gallery
 
@@ -11,6 +12,44 @@ class Medication < ActiveRecord::Base
     presence: true
   validates :ingestion_method,
     presence: true
+
+  def self.table_headings
+    return ["Date", "Name", "Instructions", "Status", "Actions"]
+  end
+
+  def table_body
+    action_view = ActionView::Base.new(Rails.configuration.paths["app/views"])
+    action_view.class_eval do
+      include Rails.application.routes.url_helpers
+      include ApplicationHelper
+
+      def protect_against_forgery?
+        false
+      end
+    end
+    if self.current === true
+      status = 'current'
+    elsif self.current === false
+      status = 'discontinued'
+    else
+      status = 'not noted'
+    end
+
+    if self.common_name != nil
+      name = "#{self.name} (#{self.common_name.upcase}) #{sprintf("%g", self.dose)} #{self.dose_unit_of_measurement} #{self.dosage_form}"
+    elsif common_name == nil
+      name = "#{self.name}"
+    end
+    return {
+      'date': self.created_at.strftime("%b %Y"),
+      'name': name,
+      'instructions': self.ingestion_method,
+      'status': status,
+      'actions': "#{action_view.render(
+        partial: 'medications/link_buttons', format: :txt,
+        locals: { m: self})}".html_safe
+    }
+  end
 
   def generate_summary
     summ = "#{self.name.downcase}"
