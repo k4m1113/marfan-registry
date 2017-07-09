4075f379560dd21b4d8bf352e33275703c9ad5cf
Kamille Delgardo <kamilledelgardo@gmail.com>
2017-07-08 19:51:13 -0700

    migration fix

--- db/migrate/20170709022218_change_test_date_attribute.rb
+++ db/migrate/20170709022218_change_test_date_attribute.rb
@@ -1,5 +1,5 @@
 class ChangeTestDateAttribute < ActiveRecord::Migration[5.1]
   def change
-    rename_column :tests, :absolute_start_date, :test_date
+    rename_column :tests, :test_date, :absolute_start_date
   end
 end
