4d8934a61e24012b2591847b778e41836e2c2f57
Kamille Delgardo <kamilledelgardo@gmail.com>
2017-07-14 20:39:54 -0700

    database fix, tgif

--- db/migrate/20170709022218_change_test_date_attribute.rb
+++
@@ -1,5 +0,0 @@
-class ChangeTestDateAttribute < ActiveRecord::Migration[5.1]
-  def change
-    rename_column :tests, :test_date, :absolute_start_date
-  end
-end
--- db/migrate/20170712234615_change_heart_meas_test_date.rb
+++
@@ -1,5 +0,0 @@
-class ChangeHeartMeasTestDate < ActiveRecord::Migration[5.1]
-  def change
-    rename_column :heart_measurements, :test_date, :absolute_start_date
-  end
-end
