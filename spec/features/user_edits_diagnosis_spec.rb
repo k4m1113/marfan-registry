c5479f0ec6228d95b01bf54aa2b55e8d8872fc15
Kamille Delgardo <kamilledelgardo@gmail.com>
2017-08-23 17:10:55 -0700

    edit diagnosis spec

--- spec/features/user_edits_diagnosis_spec.rb
+++ spec/features/user_edits_diagnosis_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 feature 'user edits diagnosis', type: :feature do
-  context 'with positive osteoporosis diagnosis' do
+  context 'positive for osteoporosis' do
     let!(:patient) {
       FactoryGirl.create(:patient,
                          first_name: 'Olive',
@@ -33,9 +33,7 @@ feature 'user edits diagnosis', type: :feature do
       expect(page).to have_content diagnosis.generate_full_summary
 
       fill_in 'diagnosis_frequency', with: '2 times per decade'
-      save_and_open_page
       page.find("button[type='submit']").click
-      save_and_open_page
       expect(page).to have_current_path patient_path(patient.id)
 
       alert = page.find('div.alert')
@@ -45,24 +43,27 @@ feature 'user edits diagnosis', type: :feature do
     scenario 'from visit page', js: true do
       visit visits_path
 
-      click_link 'Edit Visit', match: :first
+      click_button 'Edit Visit', match: :first
       expect(page).to have_current_path edit_visit_path(current_visit.id)
 
-      # expect(page).to_not have_content('No concerns noted yet')
-      #
-      # expect(page).to have_content('orthopedic concerns')
-      # page.find('a.open-tab').click
-      # edit_button = page.all('div.btn-group').last.find('a.btn', match: :first)
-      # edit_button.click
-      # expect(page).to have_current_path edit_diagnosis_path(diagnosis.id)
-      # expect(page).to have_content diagnosis.generate_full_summary
-      #
-      # fill_in 'diagnosis_frequency', with: '2 times per decade'
-      # page.find("button[type='submit']").click
-      # expect(page).to have_current_path edit_visit_path(current_visit.id)
-      #
-      # alert = page.find('div.alert')
-      # expect(alert).to have_content "#{diagnosis.generate_summary} updated for patient Oyl, Olive"
+      expect(page).to have_content('orthopedic concerns')
+      summary_bar = page.find('div.card.summary-bar')
+      summary_bar.click
+      expect(page).to have_content diagnosis.note
+      edit_button = summary_bar.all('div.btn-group').last.find('a.btn', match: :first)
+      edit_button.click
+      expect(page).to have_current_path edit_diagnosis_path(diagnosis.id)
+      expect(page).to have_content diagnosis.generate_full_summary
+
+      fill_in 'diagnosis_frequency', with: '2 times per decade'
+      page.find("button[type='submit']").click
+      expect(page).to have_current_path edit_visit_path(current_visit.id)
+
+      alert = page.find('div.alert')
+      expect(alert).to have_content "#{diagnosis.generate_summary} updated for patient Oyl, Olive"
+
+      summary_bar.click
+      expect(page).to have_content '2 times per decade'
     end
   end
 end
